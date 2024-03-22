ffa64f105cfe543ab3e9fe012769353513cce0e9
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-03-22 09:26:17 -0300

    llll

--- Aula01-Q03.c
+++ Aula01-Q03.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+
+
+char minusculo(){
+    char ch =getche();  //ler apenas um caractere
+    
+    if ((ch>='A') && (ch<='Z'))
+    {
+        return ch + 32;
+        
+    }else{
+        return ch;
+    }
+    
+
+
+}
 
 
 int main(){
 
-   
-
-
+   char letra = minusculo();
+   printf("/n%c",letra);
 
 
 
